f20754cba33c0f092f3a03b5bcc275ae41bfc0a0
VaishviBhatt <107868084+VaishviBhatt@users.noreply.github.com>
2026-09-08 14:12:31 +0100

    Add: minimum-stack - submission-8

---
+++ Data Structures & Algorithms/minimum-stack/submission-8.java
@@ -0,0 +1,31 @@
+class MinStack {
+
+    Deque<Integer> stack = new ArrayDeque<>();
+    Deque<Integer> minStack = new ArrayDeque<>();
+    
+    public void push(int val) {
+
+        if(minStack.isEmpty() || val <= minStack.peek()){
+            minStack.push(val);
+        }
+        stack.push(val);
+        
+    }
+    
+    public void pop() {
+        if(minStack.peek().equals(stack.peek())){
+            minStack.pop();
+        }
+        stack.pop(); 
+    }
+    
+    public int top() {
+        return stack.peek();
+        
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+  
+    }
+}
