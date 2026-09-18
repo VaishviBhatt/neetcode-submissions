662962e97b4a15f0de8c2bb2a42237098d2ffa3c
VaishviBhatt <107868084+VaishviBhatt@users.noreply.github.com>
2026-09-18 16:14:40 +0100

    Add: two-integer-sum-ii - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-4.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+
+        int i = 0;
+        int j = numbers.length -1;
+
+        int[] result = new int[2];
+
+        while(i<j){
+            if(i<j && numbers[i] + numbers[j] > target){
+                j--;
+            }else if( i<j && numbers[i] + numbers[j] < target){
+                i++;
+            }else {
+                result[0] = i +1;
+                result[1] = j +1;
+                return result;
+            }
+        }
+        return result;
+        
+    }
+}
